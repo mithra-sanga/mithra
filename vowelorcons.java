02d746c1db9f0a506f621e4095e1fce302ae41a5
mithra-sanga <sangamithiraikrishnan@gmail.com>
2017-07-09 12:17:26 +0530

    Add files via upload

---
+++ vowelorcons.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	char c;
+Scanner s=new Scanner(System.in);
+c=s.nextChar();
+if(C=='a'|c=='i'|c=='e'|c=='o'|c=='u'|c=='A'|c=='E'|c=='I'|c=='O'|c=='U')
+System.out.println("The given char is vowel");
+else
+System.out.println("The given char is consonant");
+
+	}
+}
