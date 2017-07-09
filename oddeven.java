762f1e8f15207fa3392ab99bc453f2e6fdf6d9fc
mithra-sanga <sangamithiraikrishnan@gmail.com>
2017-07-09 12:12:19 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	cint x1;
+Scanner s= new Scanner(System.in);
+System.out.println("Enter the number:");
+x1=s.nextInt();
+if(x1%2==0)
+System.out.println("The number is Even");
+else 
+System.out.println("The number is odd);
+	}
+}
