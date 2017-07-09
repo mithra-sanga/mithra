01470da9deb93da00458aeeb8c230e177fd40a4a
mithra-sanga <sangamithiraikrishnan@gmail.com>
2017-07-09 15:45:48 +0530

    Add files via upload

---
+++ oddlimit.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+	int x1,x2,i;
+	Scanner s= new Scanner(System.in);
+	x1=s.nextInt();
+	x2=s.nextInt();
+	for(i=x1;i<=x2;i++)
+	{
+	if(i%2!=0)
+	System.out.println("\n"+i);
+	}
+	}// your code goes here
+	
+}
+}
